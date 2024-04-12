d1a6b6791eec061c5d578961b8fee16944b0bad2
Massimo Pennazio <maxipenna@libero.it>
2024-04-12 17:10:30 +0200

    In practice we can have two consecutive x8h7_pkt_send_defer calls, fixed mutex handling

--- x8h7_drv.c
+++ x8h7_drv.c
@@ -237,11 +237,8 @@ int x8h7_pkt_send_defer(uint8_t peripheral, uint8_t opcode, uint16_t size, void 
   ret = x8h7_pkt_enq(peripheral, opcode, size, data);
   if (ret < 0) {
     printk("x8h7_pkt_enq failed with %d", ret);
-    mutex_unlock(&spidev->lock);
-    return ret;
   }
-  /* No mutex unlocking here ... */
-
+  mutex_unlock(&spidev->lock);
   return ret;
 }
 EXPORT_SYMBOL_GPL(x8h7_pkt_send_defer);
@@ -250,18 +247,15 @@ EXPORT_SYMBOL_GPL(x8h7_pkt_send_defer);
  */
 int x8h7_pkt_send_now(void)
 {
-  /* No mutex locking here ... */
   struct spidev_data *spidev = x8h7_spidev;
   int ret;
 
+  mutex_lock(&spidev->lock);
   ret = x8h7_pkt_send();
   if (ret < 0) {
     printk("x8h7_pkt_send failed with %d", ret);
-    mutex_unlock(&spidev->lock);
-    return ret;
   }
   mutex_unlock(&spidev->lock);
-
   return ret;
 }
 EXPORT_SYMBOL_GPL(x8h7_pkt_send_now);
